766a30d2c982299e87f1e35fb4213eaef18d5a1a
ravalikanamburi <101136070+ravalikanamburi@users.noreply.github.com>
2022-03-17 12:58:49 +0200

    Add project files.

---
+++ Interface/BirthdayEventContent.cs
@@ -0,0 +1,27 @@
+﻿using EmpEvents.Interface;
+using System;
+
+namespace EmpEvents.Domain.Notifications
+{
+    public class BirthdayEventContent : IEmployeeEventContent
+    {
+        public int? Id { get; set; }
+        private string? EmployeeName { get; set; }
+        private string? EmployeeLastname { get; set; }
+        private DateTime? EmployeeBirthday { get; set; }
+
+
+        public BirthdayEventContent() { }
+
+        public BirthdayEventContent(string? employeeName, string? employeeLastname)
+        {
+            EmployeeName = employeeName;
+            EmployeeLastname = employeeLastname;
+        }
+
+        public string GetContent()
+        {
+            return $"Happy Birthday {EmployeeName} {EmployeeLastname}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
